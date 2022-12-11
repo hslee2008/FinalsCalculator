567f37bdb80d1ec65eb020f85111fde7a101f399
Hyunseung Lee <hyunseunglee2008@gmail.com>
2022-12-11 15:36:01 +0900

    Add Google Analytics

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
+  // @ts-nocheck
   import AtLeast from './lib/AtLeast.svelte'
+  import { GoogleAnalytics } from "@beyonk/svelte-google-analytics";
 </script>
 
 <main>
+  <GoogleAnalytics properties={['G-PV930WQQFT']} />
+
   <div class="card">
     <AtLeast />
   </div>
