4f32a36351f0370c91ddccc2d20aac610b7899fd
Hyunseung Lee <hyunseunglee2008@gmail.com>
2023-01-10 14:24:18 +0900

    Update Calculator

--- src/lib/AtLeast.svelte
+++ src/lib/AtLeast.svelte
@@ -24,15 +24,9 @@
   let final_F = 0
 
   const change = () => {
-    if (whetherMidterm) {
-      percent = whetherMidterm ? 30 : 50
-      projectsFull = 100 - percent * 2
-      projects = projectsFull
-    } else {
-      percent = whetherMidterm ? 30 : 50
-      projectsFull = 100 - percent
-      projects = projectsFull
-    }
+    percent = whetherMidterm ? 30 : 50
+    projectsFull = 100 - percent - (whetherMidterm ? percent : 0)
+    projects = projectsFull
   }
 
   const parsePercent = (/** @type {number} */ a) =>
@@ -74,13 +68,14 @@
     whetherMidterm = e.detail === '중간있는 과목'
     change()
   }
+
+  const closeDialog = () => {
+    dialog.close()
+  }
 </script>
 
-<div style="margin-bottom: 20px">
-  <Select
-    labelText="중간"
-    on:change={ChangedSelect}
-  >
+<div class="mb20">
+  <Select on:change={ChangedSelect}>
     <SelectItem value="중간있는 과목" />
     <SelectItem value="중간없는 과목" />
   </Select>
@@ -91,15 +86,15 @@
   개발자 <a href="https://github.com/HyunseungLee-Travis/FinalsCalcualtor/blob/main/src/lib/AtLeast.svelte" rel="noreferrer" target="_blank">@이현승</a>
 </p>
 
-<div style="margin-bottom: 20px">
+<div class="mb20">
   <TextInput
     type="text"
     bind:value={percent}
-    labelText={`지필고사 전체에 대한 비율 (30, 35, 40)`}
+    labelText={`각 지필고사 전체에 대한 비율 (30, 35, 40)`}
   />
 </div>
 {#if whetherMidterm}
-  <div style="margin-bottom: 20px">
+  <div class="mb20">
     <TextInput
       type="text"
       bind:value={midterm}
@@ -107,7 +102,7 @@
     />
   </div>
 {/if}
-<div style="margin-bottom: 20px">
+<div class="mb20">
   <TextInput
     type="text"
     bind:value={projects}
@@ -124,7 +119,7 @@
 
 <Dialog bind:dialog>
   <Carousel bind:this={carousel}>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       {#if final_A > 100}
         <h1>A는 불가능</h1>
       {:else}
@@ -132,7 +127,7 @@
         <p>기말고사에서 {final_A}점 맞아야 합니다.</p>
       {/if}
     </div>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       {#if final_B > 100}
         <h1>B는 불가능</h1>
       {:else}
@@ -140,7 +135,7 @@
         <p>기말고사에서 {final_B}점 맞아야 합니다.</p>
       {/if}
     </div>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       {#if final_C > 100}
         <h1>C는 불가능</h1>
       {:else}
@@ -152,7 +147,7 @@
         {/if}
       {/if}
     </div>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       <h1>최소 D</h1>
       {#if final_D < 0}
         <p>D는 이미 확정!</p>
@@ -160,7 +155,7 @@
         <p>기말고사에서 {final_D}점 맞아야 합니다.</p>
       {/if}
     </div>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       <h1>최소 E</h1>
       {#if final_E < 0}
         <p>E는 이미 확정!</p>
@@ -168,7 +163,7 @@
         <p>기말고사에서 {final_E}점 맞아야 합니다.</p>
       {/if}
     </div>
-    <div style="margin-bottom: 5px">
+    <div class="mb5">
       <h1>최소 F</h1>
       {#if final_F < 0}
         <p>F는 이미 확정!</p>
@@ -178,9 +173,23 @@
     </div>
   </Carousel>
 
-  <div>
-    <Button outlined shaped color="Red" on:click={() => dialog.close()}>
+  <div style="margin-top: 50px;">
+    <Button outlined shaped color="Red" on:click={closeDialog}>
       닫기
     </Button>
   </div>
 </Dialog>
+
+<style>
+  h1 {
+    font-size: 2rem;
+  }
+
+  .mb5 {
+    margin-bottom: 5px;
+  }
+
+  .mb20 {
+    margin-bottom: 20px;
+  }
+</style>
