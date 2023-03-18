f3d809e4bec32b08805b38ac5f9d9a334c54e612
Hyunseung Lee <hyunseunglee2008@gmail.com>
2023-03-18 13:17:34 +0900

    Optimize Importing - Carbon Components Svelte

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { optimizeImports } from 'carbon-preprocess-svelte'
 
 export default defineConfig({
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [
+    svelte({ hot: !process.env.VITEST, preprocess: [optimizeImports()] })
+  ],
   test: {
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
