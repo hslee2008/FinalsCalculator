eade81108b922804fdf70356cc4501e5a6fb2c5f
Hyunseung Lee <hyunseunglee2008@gmail.com>
2023-04-13 19:54:55 +0900

    feat: update developer table & fix sw

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -39,7 +39,7 @@
       <table class="bx--data-table">
         <thead>
           <tr>
-            <th scope="col" data-header="grade" class="grade-row">
+            <th scope="col" data-header="grade">
               <div class="bx--table-header-label">해당 항목</div>
             </th>
             <th scope="col" data-header="lowest">
@@ -49,7 +49,7 @@
         </thead>
         <tbody aria-live="polite">
           <tr>
-            <td>마지막 업데이트</td>
+            <td>업데이트</td>
             <td>
               {new Date(document.lastModified).toISOString().substr(0, 10)}
             </td>
@@ -58,6 +58,39 @@
             <td>플랫폼</td>
             <td>{GetUserPlatform()}</td>
           </tr>
+          <tr>
+            <td>언어</td>
+            <td>{navigator.language}</td>
+          </tr>
+          <tr>
+            <td>URL</td>
+            <td>
+              {window.location.toString().replace("https://finalscalcu.web.app/",
+              "")}
+            </td>
+          </tr>
+          <tr>
+            <td>standalone</td>
+            <td>
+              {window.matchMedia && window.matchMedia('(display-mode: standalone)').matches}
+            </td>
+          </tr>
+          <tr>
+            <td>referrer</td>
+            <td>{document.referrer}</td>
+          </tr>
+          {#if "getInstalledRelatedApps" in navigator}
+          <tr>
+            <td>앱</td>
+            {#await navigator.getInstalledRelatedApps()}
+            <td>Waiting...</td>
+            {:then app}
+            <td>{app}</td>
+            {:catch error}
+            <td>{error}</td>
+            {/await}
+          </tr>
+          {/if}
         </tbody>
       </table>
     </div>
