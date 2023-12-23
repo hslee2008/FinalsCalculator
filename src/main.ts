7e58ce07c63c9506bc109fe94ed9297910dbf2d6
Hyunseung <hyunseunglee2008@gmail.com>
2023-12-23 09:53:24 +0900

    use ! & update .gitattributes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import App from "./App.svelte";
 
 const targetElement = document.getElementById("app");
 const app = new App({
-  target: targetElement ? targetElement : document.body,
+  target: targetElement!,
 });
 
 export default app;
